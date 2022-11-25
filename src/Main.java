32673d15c1d6a88d06b69addf2383ea5498f7e2d
adamajane <adamajane@gmail.com>
2022-11-25 19:45:30 +0100

    Added algorithm-method

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void algorithm(){
+        System.out.println("Hello! Welcome to our app!");
+        System.out.println("We'll be asking you");
+    }
+
+    public static void main(String[] args) {
+        algorithm();
+    }
+
+}
